4b7e09c0a0c26a328e543d162151da8897e25fc8
Ramon de la Rosa <rdl2@hi.is>
2018-11-25 22:14:54 +0000

    Uppfærði 'helpers.js' og 'list.js'

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,5 +1,33 @@
+/**
+ * Til að hreinsa börnin út úr element.
+ *
+@param {object} element Element sem á að hreinsa börn úr
+*/
+
 export function empty(element) {
   while (element.firstChild) {
     element.removeChild(element.firstChild);
   }
+}
+
+/**
+ * Býr til element og setur aukalega börn ef þau eru send með meðfylgjandi..
+ *
+ * @param {string} name Nafn á element
+ * @param  {...any} children Börn fyrir element
+ */
+export function el(name, ...children) {
+  const element = document.createElement(name);
+
+  if (Array.isArray(children)) {
+    children.forEach((child) => {
+      if (typeof child === 'string') {
+        element.appendChild(document.createTextNode(child));
+      } else if (child) {
+        element.appendChild(child);
+      }
+    });
+  }
+
+  return element;
 }
